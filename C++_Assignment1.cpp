5f79916d9cec0c1173cde97f5c51616478e74978
Shahrez7 <116435332+Shahrez7@users.noreply.github.com>
2023-06-25 16:06:53 +0500

    Final Update Done
    Final Update

--- C++_Assignment1.cpp
+++ C++_Assignment1.cpp
@@ -6,15 +6,15 @@
 #include <ctime>
 
 using namespace std;
-const int GameSize=4;
+const int GameSize=4; //size of grid 
 int score;
 int high=0;
-void board(int game[GameSize][GameSize]){
+void board(int game[GameSize][GameSize]){ //function just for outputting like a board
 	int max=GameSize;
 	int start1;
 	int start2;
 	HANDLE col;
-	col= GetStdHandle(STD_OUTPUT_HANDLE);
+	col= GetStdHandle(STD_OUTPUT_HANDLE); //to add colours
 	int col_code;
 	cout<<"Up/Down/Left/Right"<<endl;
 	
@@ -48,25 +48,26 @@ void newnum(int game[GameSize][GameSize]){
 	
 	
 	while(1){
-		newnumberrow=rand()%4;
-		newnumbercol=rand()%4;
-		srand(time(0));
-		if(game[newnumberrow][newnumbercol]==0){
+		newnumberrow=rand()%4; //random row
+		newnumbercol=rand()%4; //random column
+		srand(time(0)); //random seed
+		if(game[newnumberrow][newnumbercol]==0){ //if that point is empty
 		
 		while(1){
 		
-		newnumber=rand()%3+2;	
+		newnumber=rand()%3+2;	 //number between 2 to 4
+		 
 		if (newnumber==2){
 			    break;
 			}
-		
+			
 		else{
 			newnumber=4;
 			    break;
 			}	
 		}
 					
-		game[newnumberrow][newnumbercol]=newnumber;
+		game[newnumberrow][newnumbercol]=newnumber; //setting new number
 		break;
 		
 		}
@@ -75,23 +76,23 @@ void newnum(int game[GameSize][GameSize]){
 }
 void Up(int game[GameSize][GameSize]){
 	int a,b,c;
-	for(b=0;b<GameSize;b++){
-		int row=0;
-		for(a=0;a<GameSize;a++){
-			if (game[a][b]!=0){
-				game[row][b]=game[a][b];
+	for(b=0;b<GameSize;b++){ //through each column 
+		int row=0; //setting row index to 0
+		for(a=0;a<GameSize;a++){ //iterating through each element
+			if (game[a][b]!=0){ //if point not empty
+				game[row][b]=game[a][b]; //move it up i.e current row
 				if(row!=a){
-					game[a][b]=0;
+					game[a][b]=0; //setting it 0 if row index is differentm
 				}
 				row++;
 			}
 		}
-			for(c=0;c<3;c++){
+			for(c=0;c<3;c++){ //for merging adjacent tiles
 			
-			if (game[c][b]==game[c+1][b]){
+			if (game[c][b]==game[c+1][b]){ //if two adjacent tiles are same, merge them and set value
 				game[c][b]=game[c][b]*2;
 				game[c+1][b]=0;
-				score=score+game[c][b];
+				score=score+game[c][b]; //update score
 			
 			}
 		}			
@@ -100,23 +101,23 @@ void Up(int game[GameSize][GameSize]){
 }
 void Down(int game[GameSize][GameSize]){
 	int a,b,c;
-	for(b=0;b<GameSize;b++){
-		int row=3;
-		for(a=3;a>=0;a--){
-			if (game[a][b]!=0){
-				game[row][b]=game[a][b];
+	for(b=0;b<GameSize;b++){ //iterating through each column
+		int row=3; //setting row to 3
+		for(a=3;a>=0;a--){ //iterating through each element in reverse order
+			if (game[a][b]!=0){ //if point not empty
+				game[row][b]=game[a][b]; //move it down
 				if(a!=row){
-					game[a][b]=0;
+					game[a][b]=0; //set it 0 if row index is different
 				}
 				row--;
 			}
 		}
 		for(c=3;c>0;c--){
 		
-			if (game[c][b]==game[c-1][b]){
-				game[c][b]=game[c][b]*2;
+			if (game[c][b]==game[c-1][b]){ //merging adjacent tiles 
+				game[c][b]=game[c][b]*2; //if two are same merge them
 				game[c-1][b]=0;
-				score=score+game[c][b];
+				score=score+game[c][b]; //update score
 			}
 		}
 				
@@ -125,24 +126,24 @@ void Down(int game[GameSize][GameSize]){
 }
 void Right(int game[GameSize][GameSize]){
 	int a,b,c;
-	for(b=0;b<GameSize;b++){
-		int row=3;
-		for(a=3;a>=0;a--){
-			if (game[b][a]!=0){
-				game[b][row]=game[b][a];
+	for(b=0;b<GameSize;b++){ //iterating each row grid
+		int row=3; //setting row as 3
+		for(a=3;a>=0;a--){ //iterating each element in reverse order 
+			if (game[b][a]!=0){ //if point not empty 
+				game[b][row]=game[b][a]; //move right 
 				if(row!=a){
-					game[b][a]=0;
+					game[b][a]=0; //set 0 
 				}
 				row--;
 			}
 		}
-		for (c=3;c>0;c--){
+		for (c=3;c>0;c--){  //merge adjacent tiles 
 		
 		
-			if (game[b][c]==game[b][c-1]){
-				game[b][c]=game[b][c]*2;
+			if (game[b][c]==game[b][c-1]){ //if same value
+				game[b][c]=game[b][c]*2; //merge and double 
 				game[b][c-1]=0;
-				score=score+game[b][c];
+				score=score+game[b][c]; //update score 
 			}
 		}
 				
@@ -151,22 +152,22 @@ void Right(int game[GameSize][GameSize]){
 }
 void Left(int game[GameSize][GameSize]){
 	int a,b,c;
-	for(b=0;b<GameSize;b++){
+	for(b=0;b<GameSize;b++){ //iterate each row
 		int row=0;
-		for(a=0;a<GameSize;a++){
-			if (game[b][a]!=0){
-				game[b][row]=game[b][a];
+		for(a=0;a<GameSize;a++){ //iterate each element in row 
+			if (game[b][a]!=0){//if current tile not empty
+				game[b][row]=game[b][a];  //move left 
 				if(row!=a){
-					game[b][a]=0;
+					game[b][a]=0; //set 0 
 				}
 				row++;
 			}
 		}
-		for(c=0;c<3;c++)
-			if (game[b][c]==game[b][c+1]){
-				game[b][c]=game[b][c]*2;
+		for(c=0;c<3;c++) //merging values 
+			if (game[b][c]==game[b][c+1]){ //if adjacent tiles same 
+				game[b][c]=game[b][c]*2; //merge and double 
 				game[b][c+1]=0;
-				score=score+game[b][c];
+				score=score+game[b][c]; //update score 
 			}
 				
 		
@@ -176,27 +177,27 @@ int Over(int game[GameSize][GameSize]){
 	int x,y;
 	for (x=0;x<GameSize;x++){
 		for (y=0;y<GameSize;y++){
-			if(game[x][y]==2048){
-				cout<<"Congratulations You Won the 2048 Game"<<endl;
-				return 1;
+			if(game[x][y]==2048){ //if players makes 2048 
+				cout<<"\nCongratulations You Won the 2048 Game"<<endl;
+				return 1; //wins
 			}
 			if(game[x][y]==0){
-				return 0;
+				return 0; //if empty tile, game not over yet
 			}
 			if(game[x][y]==game[x][y+1]){
-				if(y!=3){
-					return 0;
+				if(y!=3){ //if two adjacent have same value in same row 
+					return 0; //game not over 
 				}
 			}
 			if(game[x][y]==game[x+1][y]){
-				if(x!=3){
-					return 0;
+				if(x!=3){ //if two adjacent have same value in the same column
+					return 0; //game not over 
 				}
 			}
 		}
 	}
 	cout<<"No Tiles Left"<<endl;
-	return 1;
+	return 1; //game over 
 }
 int main(){
 	int g[GameSize][GameSize];
@@ -234,21 +235,25 @@ int main(){
 	newnum(g);
 	newnum(g);
 	system("cls");
+	//g[0][0]=2048;
 	board(g);
+	
 	
 	int ch;
 	int check;
 	while(check!=1){
-		
+		check=Over(g);
     	ch=getch();
     	system("cls");
     	if (ch==72){
     		Up(g);
+    		check=Over(g);
     		newnum(g);
     		
 		}
 		else if (ch==80){
     		Down(g);
+    		check=Over(g);
     		newnum(g);
     		
 		}
@@ -258,6 +263,7 @@ int main(){
 		}
 		else if (ch==77){
     		Right(g);
+    		check=Over(g);
     		newnum(g);
 		}
 		
@@ -284,26 +290,18 @@ int main(){
 	if (again==1){
 		score=0;
 		system("cls");
-		
 		main();
 	}
 	else{
 		exit(0);
 	}
-	
-		
 	}
 	else if( option!=1 && option!=2 && option!=3){
 		cout<<"Invalid Input"<<endl;
-		cout<"\n\n\n";
+		cout<<"\n\n\n";
 		main();
 	}
 	
-	
-	
-	
-
-
-
+	return 0;
 }
 
